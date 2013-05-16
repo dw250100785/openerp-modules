8fca60729cb23ab9d84f64b6ec65e2825d14b841
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-05-16 11:31:36 +0530

    Simple spike to hook a print window via javascript

--- bahmni_sale_discount/__openerp__.py
+++ bahmni_sale_discount/__openerp__.py
@@ -8,6 +8,8 @@
     "description": """
     """,
     'data': ['sale_discount_price_view.xml','invoice_discount_view.xml','quotation_view.xml'],
+    'js': ['static/src/js/*.js'],
+    'qweb': ['static/src/xml/*.xml'],
     'demo': [],
     'auto_install': False,
     'application': True,
